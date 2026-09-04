93a3124aa38ceed7d3c205b5eed1231dc042d9e9
Navjyot_Singh <navjyotsingh80@gmail.com>
2026-09-04 17:06:42 +0000

    more changes

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,18 +1,7 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'GEMINI_API_KEY', obfuscate: true)
-  static const String geminiApiKey = _Env.geminiApiKey;
-
-  @EnviedField(varName: 'API_BASE_URL')
-  static const String apiBaseUrl = _Env.apiBaseUrl;
-
-  @EnviedField(varName: 'TOKEN_BASE_URL')
-  static const String tokenBaseUrl = _Env.tokenBaseUrl;
-
-  @EnviedField(varName: 'DEVICE_API_KEY', obfuscate: true)
-  static const String deviceApiKey = _Env.deviceApiKey;
+  // Use 'const' and 'String.fromEnvironment' to capture compile-time variables
+  //static const String apiKey = String.fromEnvironment('API_KEY');
+  static const String apiBaseUrl = String.fromEnvironment('API_BASE_URL');
+  static const String tokenBaseUrl = String.fromEnvironment('TOKEN_BASE_URL');
+  static const String deviceApiKey = String.fromEnvironment('DEVICE_API_KEY');
 }
